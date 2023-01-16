b03d8b35c64d4fdee9e91a9c41f2ab9637971d51
ShaHuss <117936918+ShaHuss@users.noreply.github.com>
2023-01-16 16:24:01 +0000

    Fixed js link not working

--- assets/Script/js/logic.js
+++ assets/Script/js/logic.js
@@ -2,13 +2,54 @@
 let questionTitle = document.querySelector("#question-title");
 let answerChoices = document.querySelector("#choices-list");
 let timeRemaining = document.querySelector("#time");
+let playerScore = 0;
+let playerCorrectAnswer = 0;
+let gameTime = 10;
 let questionsAskedMarker = 0;
-
 let clickedAnswer;
 let rightAnswer;
 
 
+
+
+function gameTimer() {
+
+  let timerInterval =setInterval(function () {
+  // timeRemaining.textContent = gameTime;
+  // gameTime--;
+  timeRemaining.textContent = gameTime;
+  gameTime--;
+  if(gameTime <0) {
+    clearInterval(timerInterval);
+
+    gameTime=0;
+
+    gameEnd();
+  } else {
+
+    timeRemaining.textContent = gameTime;
+    gameTime--;
+    
+  }
+  
+},1000)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 function gamePlay() {
+
   let question = questions[questionsAskedMarker][0];
   questionTitle.innerHTML = question;
   for (let j = 1; j < 5; j++) {
@@ -27,18 +68,39 @@ function gamePlay() {
 
 answerCheck();
 
+
+function gameEnd() {
+
+console.log("Your correct answers = "+playerCorrectAnswer);
+  
+}
+
+
+
 function correctAnswer() {
   console.log("That is correct!!");
   let correctsound = new Audio("/assets/sfx/correct.wav");
   correctsound.play();
+  playerCorrectAnswer++;
 }
+
+
 function wrongAnswer(params) {
+  gameTime=gameTime-3;
+
   console.log("That is wrong!");
   let wrongSound = new Audio("/assets/sfx/incorrect.wav");
   wrongSound.play();
 }
 
-function timer(params) {}
+
+
+
+
+
+
+
+
 
 function answerCheck() {
   answerChoices.addEventListener("click", function (event) {
@@ -67,6 +129,7 @@ let playBtn = document.querySelector("#start");
 
 playBtn.addEventListener("click", function (event) {
   event.preventDefault();
+  gameTimer();
   gamePlay();
   console.table(questions);
   document.getElementById("start-screen").innerHTML = "";
